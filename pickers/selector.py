f78f07a09d3972940982bc21e39a28930c5b3625
ocramoa <kaedon.apezteguia@gmail.com>
2023-02-20 19:50:12 -0700

    Making important comments.

--- pickers/selector.py
+++ pickers/selector.py
@@ -1,7 +1,8 @@
 import arcade
 
 class Selector(arcade.Sprite):
-
+    """A Selector hovers above the player's currently selected passenger."""
+    
     def __init__(self, filename: str = None, scale: float = 1):
         super().__init__(filename, scale)
         
